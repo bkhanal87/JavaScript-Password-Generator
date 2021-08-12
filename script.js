79a9f608a05555d90940417018caf82f304cfe59
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-08-12 15:04:25 -0400

    third commit

--- script.js
+++ script.js
@@ -9,7 +9,18 @@ const includeNumbersElement = document.getElementById
 const includeSymbolsElement = document. getElementById
 ("includeSymbols")
 const form = document.getElementById("passwordGeneratorForm")
+const passwordDisplay = document.getElementById("passwordDisplay")
 
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65,90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97,122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48,57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33,47) .concat(
+  arrayFromLowToHigh(58,64)
+) .concat(
+  arrayFromLowToHigh(91,96)
+  .concat(
+  arrayFromLowToHigh(123,126)
+)
 
 characterAmoutRange.addEventListener("input", synchCharacterAmount)
 characterAmountNumber.addEventListener("input",synchCharacterAmount)
@@ -20,8 +31,37 @@ form.addEventListener("submit", e=> {
   const includeUppercase = includeUppercaseElement.checked
   const includeNumbers = includeNumbersElement.checked
   const includeSymbols = includeSymbolsElement.checked
-  const password = generatePassword(characterAmount, includeUppercase,includeNumbers, includeSymbols)
+  const password = generatePassword(characterAmount, 
+  includeUppercase,includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
+
 })
+
+function generatePassword(characterAmount, includeUppercase,
+includeNumbers,includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat
+  (UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+
+  const passwordCharacters = []
+  for(let i = 0; i < characterAmount; i++) 
+  {
+    const characterCode = charCodes[Math.floor(Math.random() *
+    charCodes.length)]
+    passwordCharacters.push(string.fromCharcode(characterCode))
+  }
+  return passwordCharacters.join("")
+}
+
+function arrayFromLowToHigh(low,high) {
+  const array = []
+  for ( let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
 
 function synchCharacterAmount (e) {
   const value = e.target.value
