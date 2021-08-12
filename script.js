07eccf7f7cf84ffc9c09aa9b73c5d7a1d7230bbd
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-08-11 23:01:59 -0400

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+const characterAmoutRange= document.getElementById
+("characterAmountRange")
+const characterAmountNumber= document.getElementById
+("characterAmountNumber")
+const includeUppercaseElement = document.getElementById
+("includeUppercase")
+const includeNumbersElement = document.getElementById
+("includeNumbers")
+const includeSymbolsElement = document. getElementById
+("includeSymbols")
+const form = document.getElementById("passwordGeneratorForm")
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+characterAmoutRange.addEventListener("input", synchCharacterAmount)
+characterAmountNumber.addEventListener("input",synchCharacterAmount)
 
+form.addEventListener("submit", e=> {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase,includeNumbers, includeSymbols)
+})
+
+function synchCharacterAmount (e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmoutRange.value=value
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
