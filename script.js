838e99cdc28b791bf263a0bf655fb38aa1ec927f
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-08-14 18:03:56 -0400

    final commit

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ const characterAmountNumber= document.getElementById
 const includeUppercaseElement = document.getElementById
 ("includeUppercase")
 const includeNumbersElement = document.getElementById("includeNumbers")
-const includeSymbolsElement = document. getElementById("includeSymbols")
+const includeSymbolsElement = document.getElementById("includeSymbols")
 const form = document.getElementById("passwordGeneratorForm")
 const passwordDisplay = document.getElementById("passwordDisplay")
 
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65,90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97,122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48,57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33,47) .concat(
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33,47).concat(
   arrayFromLowToHigh(58,64)
-) .concat(
+).concat(
   arrayFromLowToHigh(91,96)
-  .concat(
+).concat(
   arrayFromLowToHigh(123,126)
-  ))
+  )
 
-characterAmoutRange.addEventListener("input", synchCharacterAmount)
-characterAmountNumber.addEventListener("input",synchCharacterAmount)
+characterAmountNumber.addEventListener("input", synchCharacterAmount)
+characterAmountRange.addEventListener("input", synchCharacterAmount)
 
 form.addEventListener("submit", e=> {
   e.preventDefault()
@@ -34,27 +34,24 @@ form.addEventListener("submit", e=> {
 
 })
 
-function generatePassword(characterAmount, includeUppercase,
-includeNumbers,includeSymbols) {
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
   let charCodes = LOWERCASE_CHAR_CODES
-  if (includeUppercase) charCodes = charCodes.concat
-  (UPPERCASE_CHAR_CODES)
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
   if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
   if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
 
   const passwordCharacters = []
-  for(let i = 0; i < characterAmount; i++) 
-  {
+  for (let i = 0; i < characterAmount; i++) {
     const characterCode = charCodes[Math.floor(Math.random() *
     charCodes.length)]
-    passwordCharacters.push(string.fromCharcode(characterCode))
+    passwordCharacters.push(String.fromCharCode(characterCode))
   }
   return passwordCharacters.join("")
 }
 
-function arrayFromLowToHigh(low,high) {
+function arrayFromLowToHigh(low, high) {
   const array = []
-  for ( let i = low; i <= high; i++) {
+  for (let i = low; i <= high; i++) {
     array.push(i)
   }
   return array
